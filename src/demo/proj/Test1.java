07f22caf238c9c5f449bbe9e706d8c125ed9f015
NaliniMano <nalinimano@gmail.com>
2023-07-09 16:59:31 +0530

    first commit

---
+++ src/demo/proj/Test1.java
@@ -0,0 +1,10 @@
+package demo.proj;
+
+public abstract class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Test 1 file");
+	}
+
+}
